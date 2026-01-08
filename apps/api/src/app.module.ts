466a56625cf15b067045e075a7d727717f5434f2
hoppspa-spec <hoppspa@gmail.com>
2026-01-08 18:33:57 -0300

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
-
 import { AuthModule } from "./auth/auth.module";
 import { TicketsModule } from "./tickets/tickets.module";
 
 @Module({
-  imports: [
-    AuthModule,
-    TicketsModule,
-  ],
+  imports: [AuthModule, TicketsModule],
   providers: [PrismaService],
 })
 export class AppModule {}
-
