19510c811ee04dbc82ee820a44e504f64ffe78ad
hoppspa-spec <hoppspa@gmail.com>
2026-01-08 17:35:13 -0300

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,17 +3,13 @@ import { JwtModule } from "@nestjs/jwt";
 import { PrismaService } from "./prisma.service";
 
 import { AuthModule } from "./auth/auth.module";
-import { UsersModule } from "./users/users.module";
 import { TicketsModule } from "./tickets/tickets.module";
-import { ProductsModule } from "./products/products.module";
 
 @Module({
   imports: [
     JwtModule.register({}),
     AuthModule,
-    UsersModule,
     TicketsModule,
-    ProductsModule
   ],
   providers: [PrismaService],
 })
