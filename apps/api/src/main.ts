e21d9a882998f53e71a268e04f367d519814ec16
hoppspa-spec <hoppspa@gmail.com>
2026-01-15 17:56:31 -0300

    Add files via upload

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,9 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: process.env.CORS_ORIGINS?.split(',') ?? '*',
-    credentials: true,
-  });
-
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
