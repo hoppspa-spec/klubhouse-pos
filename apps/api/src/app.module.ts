671868f4102b3ddda17ccbe44d29e81719235ff9
Ivan Carrasco <hoppspa@gmail.com>
2026-01-17 20:40:27 -0300

    Fix Prisma DI in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from "@nestjs/common";
-import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
-import { TicketsModule } from "./tickets/tickets.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [PrismaModule, AuthModule, TicketsModule],
+  imports: [PrismaModule, AuthModule],
 })
 export class AppModule {}
+
 
