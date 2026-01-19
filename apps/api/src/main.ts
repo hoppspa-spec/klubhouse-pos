c1f9ebb8388f2a5144165781725a6271f35fd2d1
Ivan Carrasco <hoppspa@gmail.com>
2026-01-19 18:48:13 -0300

    Enable CORS for web app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,13 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      "https://klubhouse-pos-web.onrender.com",
-      "http://localhost:3000"
-    ],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+  origin: [
+    "https://klubhouse-pos-web.onrender.com",
+    "http://localhost:3000"
+  ],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
   });
 
   await app.listen(process.env.PORT || 3001);
