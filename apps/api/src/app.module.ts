143fdfef34739422ba9a0f78fc79731a783b775e
Ivan Carrasco <hoppspa@gmail.com>
2026-01-22 22:11:23 -0300

    fix(api): make JwtService available globally for guards

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from "@nestjs/common";
 
 import { PrismaModule } from "./prisma/prisma.module";
+import { JwtGlobalModule } from "./auth/jwt-global.module";
+
 import { AuthModule } from "./auth/auth.module";
 import { TablesModule } from "./tables/tables.module";
 import { TicketsModule } from "./tickets/tickets.module";
 
 @Module({
-  imports: [PrismaModule, AuthModule, TablesModule, TicketsModule],
+  imports: [PrismaModule, JwtGlobalModule, AuthModule, TablesModule, TicketsModule],
 })
 export class AppModule {}
-
 
 
