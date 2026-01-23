32e51d937e66b351ca56316ca266d823002b873a
Ivan Carrasco <hoppspa@gmail.com>
2026-01-23 18:09:51 -0300

    WIP

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,15 +6,17 @@ import { JwtGlobalModule } from "./auth/jwt-global.module";
 import { AuthModule } from "./auth/auth.module";
 import { TablesModule } from "./tables/tables.module";
 import { TicketsModule } from "./tickets/tickets.module";
-
 import { UsersModule } from "./users/users.module";
 
-// ... dentro de imports:
-imports: [PrismaModule, JwtGlobalModule, AuthModule, TablesModule, TicketsModule, UsersModule],
-
-
 @Module({
-  imports: [PrismaModule, JwtGlobalModule, AuthModule, TablesModule, TicketsModule],
+  imports: [
+    PrismaModule,
+    JwtGlobalModule,
+    AuthModule,
+    TablesModule,
+    TicketsModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
 
