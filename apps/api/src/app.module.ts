ab8e378f7941da1c82646d1d8ae094fcdc2f6176
Ivan Carrasco <hoppspa@gmail.com>
2026-01-22 21:12:03 -0300

    fix(api): correct AppModule imports and export

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from "@nestjs/common";
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
-import { PrismaService } from "../prisma/prisma.service";
-import { JwtModule } from "@nestjs/jwt";
+
+import { AuthModule } from "./auth/auth.module";
+import { TablesModule } from "./tables/tables.module";
+import { TicketsModule } from "./tickets/tickets.module";
 
 @Module({
-  imports: [JwtModule.register({})],
-  controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  imports: [AuthModule, TablesModule, TicketsModule],
 })
-export class AuthModule {}
+export class AppModule {}
 
