86b0195e099c8cec97707eaf3ef8db614d76c73d
Ivan Carrasco <hoppspa@gmail.com>
2026-01-24 19:01:38 -0300

    chore(api): add health endpoint to verify deploy

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
 import { AuthModule } from "./auth/auth.module";
 import { TablesModule } from "./tables/tables.module";
 import { TicketsModule } from "./tickets/tickets.module";
 import { ProductsModule } from "./products/products.module";
 
 @Module({
-  imports: [
-    AuthModule,
-    TablesModule,
-    TicketsModule,
-    ProductsModule, // 👈 ESTE ES EL QUE FALTABA
-  ],
+  imports: [AuthModule, TablesModule, TicketsModule, ProductsModule],
+  controllers: [AppController],
 })
 export class AppModule {}
 
