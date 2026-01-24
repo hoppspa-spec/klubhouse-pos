e6be47b25b76392a9af3d17f610160ef59d24704
Ivan Carrasco <hoppspa@gmail.com>
2026-01-24 20:13:50 -0300

    fix(api): make JwtService global for AuthGuard

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
+import { JwtGlobalModule } from "./auth/jwt-global.module";
 import { AuthModule } from "./auth/auth.module";
 import { TablesModule } from "./tables/tables.module";
 import { TicketsModule } from "./tickets/tickets.module";
 import { ProductsModule } from "./products/products.module";
 
 @Module({
-  imports: [AuthModule, TablesModule, TicketsModule, ProductsModule],
-  controllers: [AppController],
+  imports: [
+    JwtGlobalModule, // 👈 CLAVE
+    AuthModule,
+    TablesModule,
+    TicketsModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
+
 
 
--- apps/api/src/auth/jwt-global.module.ts
+++ apps/api/src/auth/jwt-global.module.ts
@@ -6,9 +6,9 @@ import { JwtModule } from "@nestjs/jwt";
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET || "dev_secret_123",
-      signOptions: { expiresIn: "15m" },
     }),
   ],
   exports: [JwtModule],
 })
 export class JwtGlobalModule {}
+
