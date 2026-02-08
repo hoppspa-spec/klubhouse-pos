bdb4e9a302d12e415ac98f534c7ae47db7d709a9
Ivan Carrasco <hoppspa@gmail.com>
2026-02-08 17:36:06 -0300

    WIP

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: [
-    "https://klubhouse-pos-web.onrender.com",
-    "http://localhost:3000"
-  ],
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
+    origin: [
+      "https://klubhouse-pos-web.onrender.com",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
+
